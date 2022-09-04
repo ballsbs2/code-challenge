9788ffb37308b5e607b021ba77a7356a39cf7136
Sam Segee <ballsbs2@gmail.com>
2022-09-04 17:02:56 -0400

    Implemented Tweets

--- src/main/java/com/socialstation/codechallenge/models/Tweet.java
+++ src/main/java/com/socialstation/codechallenge/models/Tweet.java
@@ -1,12 +1,17 @@
 package com.socialstation.codechallenge.models;
 
+
 /**
  * Created by davidcolangelo on 12/16/21 for codechallenge. All rights reserved, this code
  * may be subject to copywright or other associated protections. Do not copy without
  * permission from the publisher.
  */
-public class Tweet {
-
-    //TODO - This should have all the tweet information
-
+public class Tweet{
+    String id;
+    String text;
+    
+    public Tweet(String id, String text){
+    	this.id = id;
+    	this.text = text;
+    }
 }
--- src/main/java/com/socialstation/codechallenge/services/TwitterServiceImpl.java
+++ src/main/java/com/socialstation/codechallenge/services/TwitterServiceImpl.java
@@ -1,9 +1,19 @@
 package com.socialstation.codechallenge.services;
 
 import com.socialstation.codechallenge.models.Tweet;
-import org.hibernate.cfg.NotYetImplementedException;
+
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,10 +23,44 @@ import java.util.List;
  */
 @Service
 public class TwitterServiceImpl implements TwitterService{
+	String key;
 
+	TwitterServiceImpl(){
+		try {
+			BufferedReader keyReader = new BufferedReader(new FileReader("src/main/resources/twitterKey.txt")); //pull key from src/main/resources
+			key = keyReader.readLine();
+			keyReader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
     @Override
     public List<Tweet> getTweets(String twitterUserId) {
-        // TODO - Implement as described
-        throw new NotYetImplementedException("Nothing here");
+    	String result = "";
+    	try {
+			URL url = new URL("https://api.twitter.com/2/users/15028747/mentions"); //pull required data
+			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+			connection.setRequestProperty("Authorization", String.format("Bearer %s", key));
+			BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			
+			result = in.readLine().split("\\[")[1].split("\\]")[0]; //split up data into usable information
+			
+			
+			in.close();
+		} catch (IOException e) {
+			return null;
+		} 
+    	finally {
+    		
+    	}
+    	String[] rawTweets = result.substring(1, result.length() - 1).split("\\},\\{");
+    	List<Tweet> tweets = new ArrayList<Tweet>();
+    	for(int i = 0; i < Math.min(rawTweets.length, 25); i++) { //compile as Tweet objects
+			tweets.add(new Tweet(rawTweets[i].substring(6, 25), rawTweets[i].substring(37)));
+		} 
+        
+        return tweets;
     }
 }
